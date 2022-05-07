c70cc25000e67e6d65137f8c057e252b4922bcde
neilgomez <neiladriang@gmail.com>
2022-05-07 20:42:44 +0800

    migrated to the mysql database; added Person model

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_git',
+        'USER': 'root',
+        'PASSWORD': 'Meowkey17!',
+        'HOST': 'localhost',
+        'PORT': '3306',
+
     }
 }
 
